9db5b589a96216b94a6ee41325f68782e9a1e182
nhatphat112 <127173620+nhatphat112@users.noreply.github.com>
2023-06-13 21:06:29 +0700

    feature/api_login

---
+++ src/main/java/com/cybersoft/cozastore_java21/advice/ValidattionExceptionHandlerAdvice.java
@@ -0,0 +1,6 @@
+package com.cybersoft.cozastore_java21.advice;
+
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class ValidattionExceptionHandlerAdvice extends ResponseEntityExceptionHandler {
+}
